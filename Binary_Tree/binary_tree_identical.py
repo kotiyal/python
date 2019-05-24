a9fb806b37cc4dcd134c424a68f97bf42d9969ab
saurabhkotiyal <saurabhkotiyal@gmail.com>
2019-05-24 00:14:52 -0700

    v0

---
+++ Binary_Tree/binary_tree_identical.py
@@ -0,0 +1,35 @@
+#code to find out is the binary trees are identical or not.
+
+
+#A binary tree node
+class Node:
+
+    #constructor to create a new node
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return str(self.data)
+
+
+
+
+if __name__ == '__main__':
+    root1 = Node(1)
+    root1.left = Node(2)
+    root1.right = Node(3)
+    root1.left.left = Node(4)
+    root1.left.right = Node(5)
+    root1.left.right.left = Node(8)
+    root1.left.right.left.left = Node(9)
+    root1.left.right.left.left.left = Node(10)
+    root1.right.right = Node(7)
+    root1.right.right.right = Node(11)
+
+    root2 = Node(1)
+    root2.left = Node(2)
+    root2.right = Node(3)
+    root2.left.left = Node(4)
+    root2.left.right = Node(5)
